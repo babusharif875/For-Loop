6906163a467a131862aa358936fc58a822fa2a61
babusharif875 <payelbegum96@gmail.com>
2021-03-17 22:20:32 +0600

    Loop

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-var jd1 = prompt("Enter any Text")
-document.write("hello")
+//var jd1 = prompt("Enter any Text")
+
+
+
+/*
+document.write(" increment"+"</br>")
+document.write(" "+ "</br>")
+for(var x = 1; x <= 10; x = x + 1)
+{
+
+    document.write(""+x+"</br>");
+
+}
+document.write("</br>"+" decrement"+"</br>")
+document.write(" "+ "</br>")
+
+for(var x = 10; x >= 1; x = x - 1)
+{
+    document.write(""+x+"</br>");
+
+}
+*/
+var sum = 0
+for(var x = 1; x <= 5; x = x + 1)
+{
+    sum = sum + x;
+   
+
+}
+
+
+
+
+document.write("sum = "+sum);
